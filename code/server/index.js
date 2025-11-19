358d1044e0a2c840be9ede7106955519f0c5fb1b
Luke <luke.renton@icloud.com>
2025-11-19 15:55:39 +0200

    recommitting for vercel

--- code/server/index.js
+++ code/server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+//for commit
 // Import routes
 const leagueRoutes = require('./src/routes/leagues');
 const leagueNightRoutes = require('./src/routes/leagueNights');
